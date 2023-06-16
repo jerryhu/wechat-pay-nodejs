d13798f757d814c81ddeba7ae096793cec419f95
Jerry Hu <jerryhunn@gmail.com>
2023-06-16 11:51:15 +0800

    声明文件 bug fix

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ import WechatPayDirect from "./wechat_pay_direct";
 import WechatPayPartner from "./wechat_pay_partner";
 
 export { WechatPayDirect as WechatPay, WechatPayPartner };
-
-export * from '../typings';
